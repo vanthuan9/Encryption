5a27047be093176bd63771930554b6ed47617470
Thuan Nguyen <thuan.nguyen@pomona.edu>
2018-10-06 19:49:54 -0700

    Planning and Starting Bob
    Added plans.txt containing things we need to do. Started coding Bob’s
    server.

--- Encryption/src/main/Bob.java
+++ Encryption/src/main/Bob.java
@@ -1,10 +1,51 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Bob {
-
+	
+	/**
+	 * args[0] ; Alice's public key
+	 * args[1] ; Bob's public key
+	 * args[2] ; Bob's secret key
+	 * args[3] ; port that Mallory will connect to
+	 * args[4] ; program configuration
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//check for correct # of args
+		if (args.length != 5) {
+			return;
+		}
+		
+		int portNumber = Integer.parseInt(args[3]);
+		
+		try {
+			ServerSocket bobServer = new ServerSocket(portNumber);
+			Socket clientSocket = bobServer.accept();
+			
+			BufferedReader in = new BufferedReader(
+						new InputStreamReader(clientSocket.getInputStream()));
+			
+			
+			
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println(inputLine);
+			}
+			
+			
+			
+		} 
+		catch (IOException e) {
+			//print error or smthng
+		}
+		
 
+		
 	}
 
 }
